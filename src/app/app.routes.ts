438f7b30bc54da11a590a0ad0aa47dfbd863cf2a
IsabellaPSilva <isabellapachecodasilva391@gmail.com>
2025-10-10 14:33:37 -0300

    salvando alterações locais antes do merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
   import { Routes } from '@angular/router';
   import { AgendamentosPage } from './agendamento/agendamento.page';
   import { LocalizacaoPage } from './localizacao/localizacao.page';
+  import { LocalizacaoPageP } from './localizacaoP/localizacaoP.page';
   import { ReservaPage } from './reservas/reserva.page';
   import { reservaPPage } from './reservaP/reservaP.page';
   import { CalendarioPPage } from './calendarioP/calendarioP.page';
@@ -12,6 +13,7 @@
   export const routes: Routes = [
     {path: '', component: AgendamentosPage },
     {path: 'localizacao', component: LocalizacaoPage },
+    {path: 'localizacaoP', component: LocalizacaoPage },
     {path: 'reserva', component: ReservaPage },
     {path: 'reservaP', component: reservaPPage },
     {path: 'calendarioP', component: CalendarioPPage },
@@ -24,16 +26,12 @@
     {path: 'ativar-localizacao',loadComponent: () => import('./home/ativar-localizacao.page').then((m) => m.AtivarLocalizacaoPage),},
     {path: 'perfil',loadComponent: () => import('./home/perfil.page').then(m => m.PerfilPage),},
     {path: 'agendamento-home',loadComponent: () => import('./home/agendamento-home.page').then(m => m.AgendamentoHomePage)},
-<<<<<<< HEAD
     {path: 'agendamento-home-profissional',loadComponent: () => import('./home/agendamento-home-profissional.page').then(m => m.AgendamentoHomePageProfissional)},
     {path: 'categoria',loadComponent: () => import('./home/categoria.page').then(m => m.CategoriaPage)}
 ];
-=======
-];
-
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
 export class CalendarioPPageRoutingModule {}
->>>>>>> origin/isabela
+
