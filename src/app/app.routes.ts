c61114eebb6c486f194f54206c47747def1f0033
CauaPMachado <cauapereiramachado14@gmail.com>
2025-10-13 13:51:26 -0300

    salvando alterações locais antes do merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,7 @@
   import { Routes } from '@angular/router';
   import { AgendamentosPage } from './agendamento/agendamento.page';
   import { LocalizacaoPage } from './localizacao/localizacao.page';
-<<<<<<< HEAD
-  import { LocalizacaoPPage } from 'src/localizacaoP/localizacaoP.page';
-=======
   import { LocalizacaoPPage } from './localizacaoP/localizacaoP.page';
->>>>>>> origin/isabela
   import { ReservaPage } from './reservas/reserva.page';
   import { reservaPPage } from './reservaP/reservaP.page';
   import { CalendarioPPage } from './calendarioP/calendarioP.page';
@@ -33,11 +29,7 @@
     {path: 'agendamento-home-profissional',loadComponent: () => import('./home/agendamento-home-profissional.page').then(m => m.AgendamentoHomePageProfissional)},
     {path: 'categoria',loadComponent: () => import('./home/categoria.page').then(m => m.CategoriaPage)}
 ];
-<<<<<<< HEAD
 
-
-=======
->>>>>>> origin/isabela
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
